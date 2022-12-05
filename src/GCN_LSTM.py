2470367deb855d7f7b8a891e15e9e0ae8c3731cc
richukuttan <richumania@gmail.com>
2022-12-05 00:31:52 -0500

    GCN cut 2

--- src/GCN_LSTM.py
+++ src/GCN_LSTM.py
@@ -22,26 +22,28 @@ class GCN_LSTM(nn.Module):
         self.Linear_sizes = Linear_Sizes
         
         self.lstm   = nn.LSTM(self.LSTM_input_size, self.LSTM_output_size, LSTM_num_layers, batch_first=True, dropout=dropout)
-        self.conv   = nn.ModuleList([GCNConv(input, output, improved=True) for input, output in zip([self.LSTM_output_size]+self.GCN_sizes[:-1], self.GCN_sizes)])
+        self.conv_stock   = nn.ModuleList([GCNConv(input, output, improved=True) for input, output in zip([self.LSTM_output_size]+self.GCN_sizes[:-1], self.GCN_sizes)])
+        self.conv_supplies_to   = nn.ModuleList([GCNConv(input, output, improved=True) for input, output in zip([self.LSTM_output_size]+self.GCN_sizes[:-1], self.GCN_sizes)])
+        self.conv_supplies_from   = nn.ModuleList([GCNConv(input, output, improved=True) for input, output in zip([self.LSTM_output_size]+self.GCN_sizes[:-1], self.GCN_sizes)])
         self.linear = nn.Sequential(*[nn.Linear(input, output) for input, output in zip([self.GCN_sizes[-1]]+self.Linear_sizes[:-1], self.Linear_sizes)])
 
-    def forward(self, x:torch.Tensor, edge_index:torch.IntTensor) -> torch.Tensor:
+    def forward(self, x:torch.Tensor, edge_index_stock:torch.IntTensor, edge_index_supplies_to:torch.IntTensor, edge_index_supplies_from:torch.IntTensor) -> torch.Tensor:
         """forward function
 
         Args:
             x (torch.Tensor): Tensor of dimension batch*n_stocks*time*features
-            edge_index (_type_): Tensor of dimension batch*2*edges*features
+            edge_index (_type_): Tensor of dimension batch*2*edges
         Returns:
             returns (torch.Tensor): Expected returns, dimension batch*n_stocks*time
         """
         batch, n_stocks, time, features = x.shape
         node_features = self.lstm(x.view(batch*n_stocks, time, features)).view(batch, n_stocks, time, self.LSTM_output_size)
         
-        # For conv, we have batch*time batches, each with n_stocks nodes
+        # For conv, we have batch*time batches, each with n_stocks nodesx1
         node_features  = node_features.permute(0, 2, 1, 3).view(batch*time*n_stocks, self.LSTM_output_size)
         batch_vector   = torch.arange(batch*time).repeat_interleave(n_stocks)
-        total_edges    = edge_index.max().item() + 1 #ensure this is int
-        new_edge_index = edge_index.repeat(batch*time) + total_edges*torch.arange(total_edges).repeat_interleave(total_edges)
+        total_edges    = edge_index.amax(dim=(1, 2)).item() + 1 #ensure this is int
+        new_edge_index = edge_index.repeat(time, 1, 1).view(batch, time, 2, edges) + total_edges*torch.arange(total_edges).repeat_interleave(total_edges)
         
         for i in self.conv:
             node_features = i(node_features, new_edge_index, batch=batch_vector)
---
+++ src/GCN_LSTM_without_batching.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch_geometric.nn import GCNConv
+
+from typing import Union, List
+
+class GCN_LSTM(nn.Module):
+    def __init__(self, LSTM_input_size:int, LSTM_output_size:int=16, LSTM_num_layers:int=2, GCN_sizes:Union[None, List[int]]=None,
+                 Linear_Sizes:Union[None, List[int]]=None, dropout=0.1) -> None:
+        super().__init__()
+        
+        if GCN_sizes is None:
+            GCN_sizes = [16, 16]
+        if Linear_Sizes is None:
+            Linear_Sizes = [16, 8, 4, 1]
+        
+        self.LSTM_input_size = LSTM_input_size
+        self.LSTM_output_size = LSTM_output_size
+        self.GCN_sizes = GCN_sizes
+        self.Linear_sizes = Linear_Sizes
+        
+        self.lstm   = nn.LSTM(self.LSTM_input_size, self.LSTM_output_size, LSTM_num_layers, batch_first=True, dropout=dropout)
+        self.conv_stock   = nn.ModuleList([GCNConv(input, output, improved=True) for input, output in zip([self.LSTM_output_size]+self.GCN_sizes[:-1], self.GCN_sizes)])
+        self.conv_supplies_to   = nn.ModuleList([GCNConv(input, output, improved=True) for input, output in zip([self.LSTM_output_size]+self.GCN_sizes[:-1], self.GCN_sizes)])
+        self.conv_supplies_from   = nn.ModuleList([GCNConv(input, output, improved=True) for input, output in zip([self.LSTM_output_size]+self.GCN_sizes[:-1], self.GCN_sizes)])
+        self.linear = nn.Sequential(*[nn.Linear(input, output) for input, output in zip([self.GCN_sizes[-1]]*3+self.Linear_sizes[:-1], self.Linear_sizes)])
+
+    def change_edges(self, edge, time):
+        shape = edge.shape[1]
+        total_edges    = edge.max().item() + 1 #ensure this is int
+        return edge.repeat(time, 1, 1).view(time, 2, shape) + total_edges*torch.arange(total_edges).repeat_interleave(total_edges)
+    
+    def forward(self, x:torch.Tensor, edge_index_stock:torch.IntTensor, edge_index_supplies_to:torch.IntTensor, edge_index_supplies_from:torch.IntTensor) -> torch.Tensor:
+        """forward function
+
+        Args:
+            x (torch.Tensor): Tensor of dimension n_stocks*time*features
+            edge_index_stock (torch.Tensor): Tensor of dimension 2*edges0
+            edge_index_supplies_to (torch.Tensor): Tensor of dimension 2*edges1
+            edge_index_supplies_from (torch.Tensor): Tensor of dimension 2*edges2
+        Returns:
+            returns (torch.Tensor): Expected returns, dimension n_stocks*time
+        """
+        n_stocks, time, features = x.shape
+        node_features = self.lstm(x)
+        
+        # For conv, we have batch*time batches, each with n_stocks nodesx1
+        node_features  = node_features.permute(1, 0, 2).view(time*n_stocks, self.LSTM_output_size)
+        batch_vector   = torch.arange(time).repeat_interleave(n_stocks)
+        new_edge_index_stock = self.change_edges(edge_index_stock, time)
+        new_edge_index_supplies_to = self.change_edges(edge_index_supplies_to, time)
+        new_edge_index_supplies_from = self.change_edges(edge_index_supplies_from, time)
+        
+        node_features_stock = node_features
+        for i in self.conv_stock:
+            node_features_stock = i(node_features_stock, new_edge_index_stock, batch=batch_vector)
+        
+        node_features_supplies_to = node_features
+        for i in self.conv_supplies_to:
+            node_features_supplies_to = i(node_features_supplies_to, new_edge_index_supplies_to, batch=batch_vector)
+        
+        node_features_supplies_from = node_features
+        for i in self.conv_supplies_from:
+            node_features_supplies_from = i(node_features_supplies_from, new_edge_index_supplies_from, batch=batch_vector)
+        
+        out = torch.cat([node_features_stock, node_features_supplies_to, node_features_supplies_from], 1)
+        
+        out = torch.squeeze(self.linear(node_features))
+        
+        #currently, shape batch*time*n_stocks. convert to batch, n_stocks, time
+        return out.reshape(time, n_stocks).T
+        
+        
